16c6b1129d368ceeca8367f7463313dc809a52a4
Dmitriy Smirnov <dimsmir10@gmail.com>
2025-11-26 23:04:29 +0300

    async engine, session_maker, create and drop database

---
+++ database/engine.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession, create_async_engine
+
+from database.models.base import Base
+from config import settings
+
+engine = create_async_engine(url=settings.DB_URL, echo=True)
+session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
