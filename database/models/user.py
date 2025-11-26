39c9068522b6c6a980da84efd53cd7406643eb2d
Dmitriy Smirnov <dimsmir10@gmail.com>
2025-11-26 23:35:51 +0300

    change user model

--- database/models/user.py
+++ database/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, BigInteger
+from sqlalchemy import String, BigInteger, Boolean
 
 from .base import Base
 
@@ -9,3 +9,5 @@ class User(Base):
     first_name: Mapped[str] = mapped_column(String(150), nullable=True)
     last_name: Mapped[str] = mapped_column(String(150), nullable=True)
     phone: Mapped[str] = mapped_column(String(20), nullable=True)
+
+    access_status: Mapped[bool] = mapped_column(Boolean, nullable=False)
