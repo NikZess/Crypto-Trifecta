0e59bb05d1f37b11be190e111041cdb5863a0c54
Dmitriy Smirnov <dimsmir10@gmail.com>
2025-11-27 00:59:54 +0300

    bot cmds

---
+++ common/bot_cmds_list.py
@@ -0,0 +1,7 @@
+from aiogram.types import BotCommand
+
+private = [
+    BotCommand(command="start", description="Старт | Перезапуск бота 🤖"),
+    BotCommand(command="menu", description="Меню бота 📘"),
+    BotCommand(command="subscribe", description="Купить подписку на бота 💸"),
+]
