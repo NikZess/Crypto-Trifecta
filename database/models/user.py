3dfb5bd4b51fd8f3995669d9b4800f6c8318ef94
Dmitriy Smirnov <dimsmir10@gmail.com>
2025-11-26 23:07:30 +0300

    class user (telegram model)

---
+++ database/models/user.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, BigInteger
+
+from .base import Base
+
+class User(Base):
+    
+    user_id: Mapped[int] = mapped_column(BigInteger, unique=True)
+    first_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(150), nullable=True)
+    phone: Mapped[str] = mapped_column(String(20), nullable=True)
