3b17a7353cefbc64fca659deafde65808e7a0048
Dmitriy Smirnov <dimsmir10@gmail.com>
2025-11-30 21:54:39 +0300

    create text for bot

---
+++ common/text_for_bot.py
@@ -0,0 +1,42 @@
+text_of_bot = {
+    "ru": {
+        "main_menu": "<strong>🚀 **Добро пожаловать в CryptoTrifecta!**\n</strong>"
+                    "Ваш универсальный бот-помощник по криптовалюте:\n\n"
+                
+                    "📊 **Трекер портфеля** - отслеживание прибыли и убытков\n"
+                    "🔔 **Сигналы рынка** - агрегация лучших торговых идей\n"
+                    "💵 **Курс криптовалют** - показ стоимости различных криптовалют\n\n"
+
+                    "**Доступные команды:**\n\n"
+                    "/portfolio - Управление портфелем\n"
+                    "/signals - Торговые сигналы\n"
+                    "/stats - Статистика доходности\n"
+                    "/help - Помощь и инструкции\n\n"
+
+                    "<strong>🛑🛑🛑 ** ДИСКЛЕЙМЕР ** 🛑🛑🛑\n\n</strong>"
+                    "Информация от бота — не финансовая рекомендация. \n"
+                    "Мы предоставляем только данные, решение принимает сам пользователь 🙃\n\n"
+
+                    "<b>Мы настоятельно рекомендумем вам прочитать всю информацию о боте - /help 😉</b>"
+    },
+    "en": {
+        "main_menu": "<strong>🚀 **Welcome to CryptoTrifecta!**\n</strong>"
+                    "Your all-in-one cryptocurrency assistant bot:\n\n"
+
+                    "📊 **Portfolio Tracker** - track profits and losses\n"
+                    "🔔 **Market Signals** - aggregate the best trading ideas\n"
+                    "💵 **Cryptocurrency Rates** - display the prices of various cryptocurrencies\n\n"
+
+                    "**Available commands:**\n\n"
+                    "/portfolio - Portfolio management\n"
+                    "/signals - Trading signals\n"
+                    "/stats - Profitability statistics\n"
+                    "/help - Help and instructions\n\n"
+
+                    "<strong>🛑🛑🛑 **DISCLAIMER** 🛑🛑🛑\n\n</strong>"
+                    "Information from the bot is not financial advice.\n"
+                    "We only provide data; the user makes the decision 🙃\n\n"
+
+                    "<b>We strongly recommend that you read all the information about the bot - /help 😉</b>"
+    }
+}
