7e45105108d1b5a18bc74250e3de5e93976bb499
Dmitry <157917126+NikZess@users.noreply.github.com>
2026-01-16 22:40:28 +0300

    file for crypto currencies

---
+++ common/crypto_currencies.py
@@ -0,0 +1,35 @@
+crypto_currencies = {
+    "₿ BTC - Bitcoin": "currency_1",      # Bitcoin ID: 1
+    "Ξ ETH - Ethereum": "currency_1027",  # Ethereum ID: 1027
+    "⛓ BNB - Binance Coin": "currency_1839",  # BNB ID: 1839
+    "◎ SOL - Solana": "currency_5426",    # Solana ID: 5426
+    "✕ XRP - Ripple": "currency_52",      # XRP ID: 52
+    "₳ ADA - Cardano": "currency_2010",   # Cardano ID: 2010
+    "🐕 DOGE - Dogecoin": "currency_74",  # Dogecoin ID: 74
+    "● DOT - Polkadot": "currency_6636",  # Polkadot ID: 6636
+    "⬢ MATIC - Polygon": "currency_3890", # Polygon ID: 3890
+    "🔗 LINK - Chainlink": "currency_1975", # Chainlink ID: 1975
+    "Ł LTC - Litecoin": "currency_2",      # Litecoin ID: 2
+    "❄️ AVAX - Avalanche": "currency_5805", # Avalanche ID: 5805
+    "🦄 UNI - Uniswap": "currency_7083",  # Uniswap ID: 7083
+    "🐕 SHIB - Shiba Inu": "currency_5994", # Shiba Inu ID: 5994
+    "🐸 PEPE - Pepe": "currency_24478",    # Pepe ID: 24478
+}
+
+CURRENCY_IDS = {
+    "currency_1": 1,          # BTC
+    "currency_1027": 1027,    # ETH
+    "currency_1839": 1839,    # BNB
+    "currency_5426": 5426,    # SOL
+    "currency_52": 52,        # XRP
+    "currency_2010": 2010,    # ADA
+    "currency_74": 74,        # DOGE
+    "currency_6636": 6636,    # DOT
+    "currency_3890": 3890,    # MATIC
+    "currency_1975": 1975,    # LINK
+    "currency_2": 2,          # LTC
+    "currency_5805": 5805,    # AVAX
+    "currency_7083": 7083,    # UNI
+    "currency_5994": 5994,    # SHIB
+    "currency_24478": 24478,   # PEPE
+}
