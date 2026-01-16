fc13e7a0df7a30fe125b06a04fe5d85f9855f02c
Dmitry <157917126+NikZess@users.noreply.github.com>
2026-01-16 22:41:03 +0300

    signals inline menu

--- handlers/menu_processing.py
+++ handlers/menu_processing.py
@@ -3,17 +3,23 @@ from kbds.inline import (
     get_user_settings_btns,
     get_user_portfolio_tracker_btns,
     get_user_portfolio_tracker_addmenu_btns,
+    get_user_market_signals_menu_btns,
+    get_user_market_currency_signals_and_btn_to_back,
 )
 
 from common.text_for_bot import text_of_bot
 
 from parser import get_currency_price_by_currency_id
 
+from common.crypto_currencies import CURRENCY_IDS, crypto_currencies
+
 async def main_menu(level: int):
     description = text_of_bot["ru"]["main_menu"]
     kbds = get_user_main_btns(level=level)
 
     return description, kbds
+
+# --------------------------------------------------------------- 
 
 
 async  def portfolio_menu(level: int):
@@ -29,6 +35,22 @@ async def portfolio_add_currency_menu(level: int):
 
     return description, kbds
 
+# ----------------------------------------------------------------------
+
+async def signals_menu(level: int):
+    description = text_of_bot["ru"]["signals_menu"]
+    kbds = get_user_market_signals_menu_btns(level=level)
+    
+    return description, kbds
+
+
+async def signals_get_currency_menu(level: int, currency_id: int):
+    description = await get_currency_price_by_currency_id(currency_id)
+    kbds =  get_user_market_currency_signals_and_btn_to_back(level=level)
+
+    return description, kbds
+
+# ----------------------------------------------------------------------
 
 async def settings_menu(level: int):
     description = await get_currency_price_by_currency_id(currency_id=1)
@@ -37,8 +59,12 @@ async def settings_menu(level: int):
     return description, kbds
 
 
+# ------------------------------------------------------------------------------
+
+
 async def get_menu_content(
     level: int,
+    menu_name: str = None
 ):
     if level == 0:
         return await main_menu(level=level)
@@ -46,5 +72,12 @@ async def get_menu_content(
         return await portfolio_menu(level=level)
     if level == 1.1:
         return await portfolio_add_currency_menu(level=level)
+    if level == 2:
+        return await signals_menu(level=level)
+    if level == 2.1 and menu_name:
+        if menu_name in CURRENCY_IDS:
+            currency_id = CURRENCY_IDS[menu_name]
+        return await signals_get_currency_menu(level=level, currency_id=currency_id)
+
     if level == 5.0:
         return await settings_menu(level=level)
